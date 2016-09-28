f73f2cbe242e501fa430068c29727706747078dc
Thomas Alrek <thomas@alrek.no>
2016-09-28 13:30:05 +0200

    Update readme

--- readme.md
+++ readme.md
@@ -1,11 +1,113 @@
-# Waveform-JS
+# JQueryWaveform
 
 A JQuery plugin written for CloudAudio
 
 
-This plugin draw a waveform on a canvas, based on an array of values (ranging from 0.0 to 1.0).
+### This plugin draw a waveform on a canvas from data created with [wav2json](https://github.com/beschulz/wav2json)
 
 
-It is very customizable!
+### [Live demo](http://alrek.no/demos/jquery-waveform/)
 
-More documentation will be added soon
+## Usage
+
+JQueryWaveform is simple to use!
+
+It works on any canvas elements.
+
+Be sure that JQuery is loaded before JQueryWaveform.
+
+JQueryWaveform is fully responsive. If you have set the canvas element to 100% width, it will rescale automatically when the windwow dimensions changes.
+
+If the canvas is smaller then the amount of sample data, it will lose some precision (it will not show all available samples), and if the amount of samples is less then the canvas width, it will interpolate the missing values.
+
+To initialize it, just call it with the following function
+
+`$('#my-canvas').waveform(options)`
+
+Also note that this returns an object. So if you want to handle the audio controls yourself, you will have to save this object in a variable for further event binding.
+
+## Options
+
+The options are very important. Without passing any, it won't work. 
+
+Some are optional, some are required
+
+#### Example
+```
+var options = { 
+   data: {"left": [0.1, 0.2, 0.3, 0.4]}
+}
+```
+
+Above is the minimum required options. The data property is in the same format as wav2json. If the data does not contain a "left" property, or you want to specify another channel, pass in this option
+
+#### Example
+```
+var options = { 
+    data: {"right": [0.1, 0.2, 0.3, 0.4]}, 
+    dataIndex: "right" 
+}
+```
+
+## Audio 
+
+As default, if you pass in a Audio element (JQuery selector), the waveform will automatically add a play/pause handler, and an onClick handler for seeking. 
+
+If you don't want this, or you want to customize how this is done, you can pass in your own  handlers.
+
+#### Example
+```
+var options = { 
+    onClick: function(e){ 
+        alert("Clicked waveform at X: " + e.offsetX); 
+    }
+    onMouseMove: function(e){ 
+        $("#move-x").html("X: " + e.offsetX); 
+        $("#move-y").html("Y: " + e.offsetY); 
+    }, 
+    onMouseOut: function(e){ 
+        $("#move-x").html("X: Outside"); 
+        $("#move-y").html("Y: Outside"); 
+        } 
+    }
+}
+```
+
+## Colors
+
+Colors are fully customizable, and can be any valid CSS color format: 
+* "red" 
+* #f00
+* #ff0000
+* rgb(255,0,0)
+* rgba(255,0,0,1)
+
+#### Example
+```
+var options = { 
+    color: "#rgb(255,0,0)", 
+    backgroundColor: "rgba(0,0,0,0)", 
+    cursorColor: "#0f0", 
+    scrubColor: "#0000ff", 
+    indicatorColor: "yellow" } 
+```
+
+## Valid options
+
+* audio
+* backgroundColor
+* color
+* cursor
+* cursorColor
+* data
+* dataIndex
+* height
+* indicatorColor
+* lineWidth
+* offset
+* onClick
+* onMouseMove
+* onMouseOut
+* scrubColor
+* softclip
+* width
--- waveform.js
+++ waveform.js
@@ -3,12 +3,17 @@
     function waveform(canvas, options){
 
         var ctx = canvas.getContext("2d");
+        
+        var waveObj = this;
 
-        var data = options.data;
+        var data = options.data[options.dataIndex];
         var width = canvas.width;
         var height = canvas.height;
         var center = (height / 2);
         var ratio = 0;
+        var cursor;
+        var playback;
+        var audio;
 
         if(options.softclip){
             var largest = Math.max.apply(Math, data);
@@ -19,18 +24,36 @@
             }
         }
         function bind(){
-            $(window).resize(resize);
-            $(canvas).click(options.onClick);
+            $(window).resize(function(){
+                resize(false);
+            });
+            $(canvas).click(function(event){
+                options.onClick(event, options);
+            });
             $(canvas).mousemove(function(event){
                 mouseMove(event, options.onMouseMove);
             });
             $(canvas).mouseout(function(event){
-                render({x: 0});
+                cursor = {x: 0, y: 0};
+                render();
+                if(typeof options.onMouseOut === 'function'){
+                    options.onMouseOut(event);
+                }
             });
+            if(typeof options.audio !== 'undefined'){
+                audio = options.audio.get(0);
+                audio.ontimeupdate = function(e){
+                    playback = {x: this.currentTime * canvas.width / this.duration};
+                    render();
+                }
+            };
         }
 
-        function resize(){
-            console.log("Resize triggered");
+        function resize(initial){
+            if(!initial){
+                options.width = $(canvas).outerWidth();
+                options.height = $(canvas).outerHeight();
+            }
             canvas.width = options.width;
             canvas.height = options.height;
             canvas.setAttribute("width", options.width.toString());
@@ -49,10 +72,11 @@
 
         function mouseMove(e, callback){
 
-            render({x: e.pageX, y: e.pageY});
+            cursor = {x: e.offsetX, y: e.offsetY};
+            render();
 
             if(typeof callback === 'function'){
-                callback();
+                callback(e);
             }
         }
 
@@ -68,12 +92,18 @@
             return Math.floor(val / ratio);
         }
 
-        function render(cursor){
+        function render(){
 
             clear();
 
             if(typeof cursor === 'undefined'){
                 cursor = 0;
+            }
+            
+            if(typeof playback === 'undefined'){
+                playback = {
+                    x: 0.0
+                }
             }
 
             for(var x = 0; x < (width * options.offset); x += options.offset){
@@ -82,39 +112,85 @@
                 sample += ratio;
 
                 /* Clip the sample */
-                if(sample > 0.99){
-                    sample = 0.99;
+                if(sample > 1){
+                    sample = 1;
                 }
 
                 ctx.beginPath();
                 ctx.lineWidth = options.lineWidth;
                 ctx.moveTo(x, center + center * (sample));
                 ctx.lineTo(x, center - center * (sample));
+
+                ctx.strokeStyle = options.color;
                 if(options.cursor && cursor.x > 0 && x <= cursor.x){
-                    ctx.strokeStyle = options.cursorColor;
-                }else{
-                    ctx.strokeStyle = options.color;
+                    if(options.cursor){
+                        if(cursor.x > playback.x){
+                            ctx.strokeStyle = options.cursorColor;   
+                        }
+                    }
                 }
+                if(typeof audio !== 'undefined'){
+                    if (audio.duration > 0 && !audio.paused) {
+                        if(playback.x > 0 && x <= playback.x){
+                            ctx.strokeStyle = options.indicatorColor;
+                        }
+                        if(options.cursor){
+                            if(x > 0 && cursor.x <= x + options.offset / 2 && cursor.x >= x - options.lineWidth / 2){
+                                ctx.strokeStyle = options.scrubColor;                       
+                            }
+                        }
+                    }else{
+                        ctx.strokeStyle = options.cursorColor;
+                    }
+                }else{
+                    if(options.cursor){
+                        if(x > 0 && cursor.x <= x + options.offset / 2 && cursor.x >= x - options.lineWidth / 2){
+                            ctx.strokeStyle = options.scrubColor;                       
+                        }
+                    }
+                }
+
                 ctx.stroke();
             }
 
         };
 
-        init();
+        init(true);
 
     }
 
     $.fn.waveform = function(options) {
 
         var settings = $.extend({
-            color: "#fff",
-            backgroundColor: "#000",
+            color: "rgba(55,55,55,1)",
+            backgroundColor: "transparent",
+            scrubColor: "rgba(0,0,0,1)",
+            indicatorColor: "rgba(240,80,111,1)",
+            cursorColor: "rgba(131,131,131,1)",
+            cursor: false,
             width: this.width,
             height: this.height,
-            lineWidth: 4,
-            offset: 10,
-            onClick: function(){},
-            onMouseMove: function(){}
+            softclip: false,
+            lineWidth: 3,
+            offset: 4,
+            onClick: function(e, waveObj){
+                if(typeof options.audio !== 'undefined'){
+                    var audio = $(options.audio).get(0);
+                    if (audio.duration > 0 && !audio.paused) {
+                        var x = e.offsetX;
+                        x = (audio.duration / waveObj.width * x);
+                        audio.currentTime = x;
+                    }else{
+                        audio.play();
+                    }
+                }
+            },
+            onMouseMove: function(){},
+            audio: undefined,
+            data: {
+                "left": [0]
+            },
+            dataIndex: "left"
         }, options);
 
         waveform(this.get(0), settings);
